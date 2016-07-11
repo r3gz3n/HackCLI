032c6b0e5c053be9bd03697b51ea845434a70dad
r3gz3n <akashmailbox722@gmail.com>
2016-07-11 05:34:31 +0530

    Added a Validator

--- install.py
+++ install.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python3
-import subprocess
+from os.path import expanduser
+from subprocess import call
+
+HOME = expanduser("~")
 
 def install():
     command = ['sudo', 'cp', './he', '/usr/local/bin/he']
-    subprocess.call(command)
+    call(command)
+    command = ['touch', HOME + '/.he_conf']
+    call(command)
+    setting = 'EDITOR:None\nBROWSER:None\nFILEPATH:None\nTEMPLATE:None\nLANG:None\nCLIENT_ID:None\nCLIENT_SECRET:None\n'
+    with open(HOME + '/.he_conf', 'w') as conf:
+        conf.write(setting)
     command = ['he', 'settings']
-    subprocess.call(command)
+    call(command)
 
 
 if __name__ == "__main__":
